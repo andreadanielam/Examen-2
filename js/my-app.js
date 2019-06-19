505430bddb6000885109c4976e1edfc988cdfe57
andreadanielam <47798435+andreadanielam@users.noreply.github.com>
2019-06-18 19:20:04 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,61 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/index/',
+    	url: 'index.html',
+    	name: 'index',
+  		},
+		{
+		path: '/artist/',
+    	url: 'artist.html',
+    	name: 'artist',
+  		},
+		{
+		path: '/playlist/',
+    	url: 'playlist.html',
+    	name: 'playlist',
+  		},
+		{
+		path: '/menu/',
+    	url: 'menu.html',
+    	name: 'menu',
+  		},
+		{
+		path: '/info/',
+    	url: 'info.html',
+    	name: 'info',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/form/',
+    	url: 'form.html',
+    	name: 'form',
+  		}
+		
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
